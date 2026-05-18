e8b5c55b43808f497e0a58cc9f13f9766fd7275d
Amarnadh <chowdaryamarmadala@gmail.com>
2026-05-18 18:06:49 +0530

    added cart total

--- bro-fries/src/pages/Cart.jsx
+++ bro-fries/src/pages/Cart.jsx
@@ -10,6 +10,13 @@ function Cart() {
     removeFromCart
   } = useContext(CartContext);
 
+  // Total Price Calculation
+  const total = cartItems.reduce(
+    (sum, item) =>
+      sum + item.price * item.quantity,
+    0
+  );
+
   return (
 
     <div
@@ -28,14 +35,30 @@ function Cart() {
         <div
           key={item.name}
           style={{
-            marginBottom: "20px"
+            marginBottom: "20px",
+            background: "#111827",
+            padding: "20px",
+            borderRadius: "15px"
           }}
         >
 
           <h2>{item.name}</h2>
 
           <h3>
+            Price: ₹{item.price}
+          </h3>
+
+          <h3>
             Quantity: {item.quantity}
+          </h3>
+
+          <h3
+            style={{
+              color: "#ffb400"
+            }}
+          >
+            Item Total:
+            ₹{item.price * item.quantity}
           </h3>
 
           <button
@@ -58,6 +81,28 @@ function Cart() {
         </div>
 
       ))}
+
+      {/* Final Total */}
+
+      <div
+        style={{
+          marginTop: "40px",
+          background: "#111827",
+          padding: "25px",
+          borderRadius: "20px",
+          textAlign: "center"
+        }}
+      >
+
+        <h1
+          style={{
+            color: "#ffb400"
+          }}
+        >
+          Total: ₹{total}
+        </h1>
+
+      </div>
 
     </div>
 
